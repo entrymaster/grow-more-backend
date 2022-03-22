1b6110d2aeb47466176859a3b1d7e23355c55979
Ayush Srivastava <ayush@onecorp.co.in>
2022-03-22 22:01:18 +0530

    new

--- routes/v1/model.route.js
+++ routes/v1/model.route.js
@@ -6,7 +6,7 @@
 // const httpStatus = require("http-status");
 
 
-async function callbackfxn(req, res) {
+// async function callbackfxn(req, res) {
     // var arr=["./python/ml_model.py"];
     // (req.body.arr).map((elem)=>{
     //     arr.push(elem);
@@ -16,28 +16,28 @@ async function callbackfxn(req, res) {
     // console.log(req.user)
     // childProcess.stdout.on('data', async function (data) {
     //     let vx=data.toString();
-        let userElem=await User.findOne({email:req.user.email});
-        // let obj={details:[...(req.body.arr)], cropSuggested:vx};
-        // console.log()
-        await userElem.history.push(req.body.value);
-        userElem.save()
+        // let userElem=await User.findOne({email:req.user.email});
+        // // let obj={details:[...(req.body.arr)], cropSuggested:vx};
+        // // console.log()
+        // await userElem.history.push(req.body.value);
+        // userElem.save()
 
-        res.send("Sucessfully added!");
+        // res.send("Sucessfully added!");
     // })
     // childProcess.stderr.on('data', function(data) {
     //     console.log('runs:', data.toString('utf8'))
     // })
-}
+// }
 
-async function newcallback(req, res){
-    let userElem=await User.findOne({email:req.user.email});
-    let hist=await userElem.history;
-    console.log(hist);
-    res.status(httpStatus.FOUND).json({history:hist});
-}
+// async function newcallback(req, res){
+//     let userElem=await User.findOne({email:req.user.email});
+//     let hist=await userElem.history;
+//     console.log(hist);
+//     res.status(httpStatus.FOUND).json({history:hist});
+// }
 
 
-router.post('/history', auth(), callbackfxn);
-router.get('/history', auth(), newcallback);
+// router.post('/history', auth(), callbackfxn);
+// router.get('/history', auth(), newcallback);
 
-module.exports = router;
+// module.exports = router;
