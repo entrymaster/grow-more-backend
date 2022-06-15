a5bbc6b77327c2241edb0108ed7b75f45efbe57c
Ayush Srivastava <ayush@onecorp.co.in>
2022-06-15 19:06:54 +0530

    ok

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -2,14 +2,14 @@ const express = require("express");
 const router = express.Router();
 const CropRoute=require("./crops.route")
 const authRoute=require("./auth.route")
-// const modelRoute=require("./model.route")
+const modelRoute=require("./model.route")
 
 // router.get("/", (req, res)=>{
 //     res.send("Faisal");
 // })
 router.use("/crops", CropRoute);
 router.use("/auth", authRoute);
-// router.use("/model", modelRoute);
+router.use("/model", modelRoute);
 
 
 
--- routes/v1/model.route.js
+++ routes/v1/model.route.js
@@ -1,43 +1,32 @@
-// var express = require('express');
-// const router = express.Router();
-// const auth=require('../../middleware/auth');
-// // var spawn = require("child_process").spawn;
-// const User=require("../../model/User")
-// const httpStatus = require("http-status");
+var express = require('express');
+const router = express.Router();
+const auth=require('../../middleware/auth');
+const User=require("../../model/User")
+const httpStatus = require("http-status");
 
 
-// async function callbackfxn(req, res) {
-    // var arr=["./python/ml_model.py"];
-    // (req.body.arr).map((elem)=>{
-    //     arr.push(elem);
-    // })
-    // console.log('Array:', arr);
-    // var childProcess = spawn('python', arr);
-    // console.log(req.user)
-    // childProcess.stdout.on('data', async function (data) {
-    //     let vx=data.toString();
-        // let userElem=await User.findOne({email:req.user.email});
-        // // let obj={details:[...(req.body.arr)], cropSuggested:vx};
-        // // console.log()
-        // await userElem.history.push(req.body.value);
-        // userElem.save()
+async function callbackfxn(req, res) {
+   
+        let userElem = await User.findOne({email: req.user.email});
+        let obj = {details: req.body.details, cropSuggested: req.body.cropSuggested}
+        await userElem.history.push(obj);
+        userElem.save()
 
-        // res.send("Sucessfully added!");
-    // })
-    // childProcess.stderr.on('data', function(data) {
-    //     console.log('runs:', data.toString('utf8'))
-    // })
-// }
+        res.status(httpStatus.CREATED).send({
+            message: 'success'
+        })
+   
+}
 
-// async function newcallback(req, res){
-//     let userElem=await User.findOne({email:req.user.email});
-//     let hist=await userElem.history;
-//     console.log(hist);
-//     res.status(httpStatus.FOUND).json({history:hist});
-// }
+async function newcallback(req, res){
+    let userElem=await User.findOne({email:req.user.email});
+    let hist=await userElem.history;
+    console.log(hist);
+    res.status(httpStatus.FOUND).json({history:hist});
+}
 
 
-// router.post('/history', auth(), callbackfxn);
-// router.get('/history', auth(), newcallback);
+router.post('/history', auth(), callbackfxn);
+router.get('/history', auth(), newcallback);
 
-// module.exports = router;
+module.exports = router;
