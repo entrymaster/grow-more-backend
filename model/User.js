e99186b205c4918887a0da1803f02f40b8fb7d59
Ayush Srivastava <ayush@onecorp.co.in>
2022-06-16 19:07:40 +0530

    update

--- model/User.js
+++ model/User.js
@@ -35,7 +35,7 @@ const userSchema = new Schema({
 userSchema.pre('save', function (next) {
   var user = this;
 
-  // if(!user.isModified('password')) return next();
+  if(!user.isModified('password')) return next();
 
   bcrypt.genSalt(10, function (err, salt) {
     if (err) return next(err);
--- routes/v1/model.route.js
+++ routes/v1/model.route.js
@@ -14,6 +14,7 @@ async function callbackfxn(req, res) {
 
         res.status(httpStatus.CREATED).send({
             message: 'success'
+            
         })
    
 }
